c44e027ed36abe93cf1613b21bc7c256e513a27c
pounder-er <75197663+pounder-er@users.noreply.github.com>
2021-02-07 01:23:57 +0700

    +Header+table+Pagination

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -18,13 +18,13 @@ class Login extends React.Component {
     }
     render() {
         return (
-            <div className="Login">
-                <body className="Container">
-                <div className="image">
-                <img className="imdog" src={logo} alt="Dogecoin"/>
+            <div className="Container">
+                <body className="Login">
+                <div className="Image">
+                <img className="Logo" src={logo} alt="Dogecoin"/>
                 </div>
 
-                    <InputGroup style={{marginTop:10}}>
+                    <InputGroup style={{marginTop:15}}>
                         <InputGroupAddon addonType="prepend">
 
                             <InputGroupText><BsFillPersonFill/></InputGroupText>
@@ -32,14 +32,14 @@ class Login extends React.Component {
                         <Input placeholder="username"  />
                     </InputGroup>
 
-                    <InputGroup style={{marginTop:10}}>
+                    <InputGroup style={{marginTop:12}}>
                         <InputGroupAddon addonType="prepend">
                             <InputGroupText><BsFillLockFill/></InputGroupText>
                         </InputGroupAddon>
                         <Input placeholder="Password" type="password"/>
                     </InputGroup>
                
-                    <Button style={{marginTop:10}} color="primary">Login</Button>
+                    <Button style={{marginTop:15}} color="primary">Login</Button>
                 </body>
             </div>
         );
