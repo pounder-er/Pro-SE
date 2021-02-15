735fa1508368b15cc2a713babe8ef2baa84dc33f
Pichaya Suwan <pichaya.suw@ku.th>
2021-02-15 13:12:39 +0700

    sawasdee naa

---
+++ my-app/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+
+import Login from './Login';
+import Contact from './Contact';
+import CompanyDetail from './CompanyDetail'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+}
+
+  render(){
+    return (
+
+
+      //<Login/>
+      <Contact/>
+      //<CompanyDetail/>
+      
+        // <header className="App-header">
+        //   <img src={logo} className="App-logo" alt="logo" />
+        //   <p>
+        //     44kl4 <code>src/App.js</code> and save to reload.
+           
+        //   </p>
+        //   <a
+        //     className="App-link"
+        //     href="https://reactjs.org"
+        //     target="_blank"
+        //     rel="noopener noreferrer"
+        //   >
+        //     Learn React
+        //   </a>
+        // </header>
+     
+    );
+  }
+}
+export default App;
