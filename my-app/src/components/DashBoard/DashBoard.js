62ef984e2c91752de7d4c1e7c6a05ebc8d6de43f
Pichaya Suwan <59646797+Pichaya-Suwan@users.noreply.github.com>
2021-04-07 18:53:11 +0700

    pine

--- my-app/src/components/DashBoard/DashBoard.js
+++ my-app/src/components/DashBoard/DashBoard.js
@@ -28,8 +28,10 @@ import {
 // import 'react-pro-sidebar/dist/css/styles.css';
 // import './CustomSideBar.scss';
 
-import { MdMenu,
-         MdArchive } from "react-icons/md";
+import {
+    MdMenu,
+    MdArchive
+} from "react-icons/md";
 import { IoMdAlarm } from "react-icons/io";
 
 import {
@@ -40,10 +42,12 @@ import {
     BsFillPeopleFill,
     BsSearch,
     BsCheckAll
-  } from "react-icons/bs";
+} from "react-icons/bs";
 
-import { AiOutlineLineChart,
-         AiFillCalculator } from "react-icons/ai";
+import {
+    AiOutlineLineChart,
+    AiFillCalculator
+} from "react-icons/ai";
 
 import Chart from "react-apexcharts";
 import { Line, Pie } from '@reactchartjs/react-chart.js'
@@ -55,41 +59,68 @@ class DashBoard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lineChartData : {
-                labels : ['จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์', 'อาทิตย์'],
+            lineChartData: {
+                labels: ['จันทร์', 'อังคาร', 'พุธ', 'พฤหัสบดี', 'ศุกร์', 'เสาร์', 'อาทิตย์'],
                 datasets: [
                     {
                         label: 'สินค้าเข้า',
-                        data : [10, 20, 30, 30, 50, 30, 20],
-                        fill : false,
+                        data: [10, 20, 30, 30, 50, 30, 20],
+                        fill: false,
                         backgroundColor: 'rgb(255, 99, 132)',
                         borderColor: 'rgba(255, 99, 132, 0.6)',
                     },
                     {
                         label: 'สินค้าออก',
-                        data : [40, 70, 60, 20, 40, 50, 60],
-                        fill : false,
+                        data: [40, 70, 60, 20, 40, 50, 60],
+                        fill: false,
                         backgroundColor: 'rgb(44, 232, 143)',
                         borderColor: 'rgba(44, 232, 143, 0.6)',
                     }
                 ]
             },
-            options : {
+            options: {
                 scales: {
-                  yAxes: [
-                    {
-                      ticks: {
-                        beginAtZero: true,
-                      },
-                    },
-                  ],
+                    yAxes: [
+                        {
+                            ticks: {
+                                beginAtZero: true,
+                            },
+                        },
+                    ],
                 },
             },
 
-            pineChartOption : {
+            pineChartData: {
+                labels: ['ข้าว1', 'ข้าว2', 'ข้าว3', 'ข้าว4', 'ข้าว5', 'ว่าง'],
+                datasets: [
+                    {
+                        label: 'สัดส่วนโกดัง',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                        ],
+                        borderWidth: 1,
+                    },
+                ],
+            },
+
+            pineChartOption: {
                 series: [40, 20, 10, 30],
                 labels: ['ข้าว1', 'ข้าว2', 'ข้าว3', 'ว่าง'],
-                
+
             }
         }
     }
@@ -97,14 +128,16 @@ class DashBoard extends React.Component {
 
 
     render() {
-        return (                
-                    <body className="DBBody">
+        return (
+            <body className="DBBody">
 
-                        <div className="lineChart">
-                            <h3 style={{justifySelf:'flex-start', 
-                                      
-                                        marginTop: 10}}>จำนวนออร์เดอร์สินค้าเข้า/ออก</h3>
-                            {/* <Chart
+                <div className="lineChart">
+                    <h3 style={{
+                        justifySelf: 'flex-start',
+
+                        marginTop: 10
+                    }}>จำนวนออร์เดอร์สินค้าเข้า/ออก</h3>
+                    {/* <Chart
                                 options={this.state.options}
                                 series={this.state.series}
                                 // type="line"
@@ -113,18 +146,20 @@ class DashBoard extends React.Component {
                                 style={{alignSelf:'center',
                                         backgroundColor: "white"}}
                             /> */}
-                            <Line data={this.state.lineChartData} 
-                                  options={this.state.options}
-                               
-                                  height={70}/>
-                        </div>
-                        <div className="rowContainer">
-                            <div className="donutChartContainer">
-                                <div className="pineChart">
-                                    <h3 style={{justifySelf:'flex-start', 
-                                                marginLeft:'5%', 
-                                                marginTop: 20}}>สัดส่วนเนื้อที่โกดัง</h3>
-                                    <Chart
+                    <Line data={this.state.lineChartData}
+                        options={this.state.options}
+
+                        height={70} />
+                </div>
+                <div className="rowContainer">
+                    <div className="donutChartContainer">
+                        <div className="pineChart">
+                            <h3 style={{
+                                justifySelf: 'flex-start',
+                                marginLeft: '5%',
+                                marginTop: 20
+                            }}>สัดส่วนเนื้อที่โกดัง</h3>
+                            {/* <Chart
                                         options={this.state.pineChartOption}
                                         series={this.state.pineChartOption.series}
                                         type="donut"
@@ -134,42 +169,46 @@ class DashBoard extends React.Component {
                                                 backgroundColor: "white",
                                                 marginBottom:20,
                                                 marginTop:10}}
-                                    />
-                                </div>
-                            </div>
-                            <div className="cardContainer">
-                                <Card style={{borderColor:"transparent", 
-                                            width:"97%", marginLeft:'3%'}}>
-                                    <p/>
-                                    <h3 style={{marginLeft:'2.5%'}}>สินค้าขายดี</h3>
-                                    <p/>
-                                    <Table striped style={{width:'95%', alignSelf:'center'}}>
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>รหัสสินค้า</th>
-                                                <th>ชื่อสินค้า</th>
-                                                <th>ปริมาณ</th>
-                                                <th>มูลค่า(บาท)</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>110101</td>
-                                                <td>ข้าวขาวดี</td>
-                                                <td>1000</td>
-                                                <td>1,000,000</td>
-                                            </tr>
-                                        
-                                        </tbody>
-                                    </Table>
-                                </Card>
-                            </div>
+                                    /> */}
+                            <Pie data={this.state.pineChartData}
+                                height={300} />
                         </div>
-                       
-                        
-                    </body>
+                    </div>
+                    <div className="cardContainer">
+                        <Card style={{
+                            borderColor: "transparent",
+                            width: "97%", marginLeft: '3%'
+                        }}>
+                            <p />
+                            <h3 style={{ marginLeft: '2.5%' }}>สินค้าขายดี</h3>
+                            <p />
+                            <Table striped style={{ width: '95%', alignSelf: 'center' }}>
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>รหัสสินค้า</th>
+                                        <th>ชื่อสินค้า</th>
+                                        <th>ปริมาณ</th>
+                                        <th>มูลค่า(บาท)</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>110101</td>
+                                        <td>ข้าวขาวดี</td>
+                                        <td>1000</td>
+                                        <td>1,000,000</td>
+                                    </tr>
+
+                                </tbody>
+                            </Table>
+                        </Card>
+                    </div>
+                </div>
+
+
+            </body>
 
         );
     }
