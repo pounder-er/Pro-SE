737dbae2e8cc7aabf794b6f25dee6752c139cc23
kitsadapan <75611670+kitsadapan@users.noreply.github.com>
2021-04-07 17:05:47 +0700

    Update EditCalculate.js

--- my-app/src/components/Calculate/EditCalculate.js
+++ my-app/src/components/Calculate/EditCalculate.js
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import {
-    Form,
+    Button,
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText,
     FormGroup,
     Label,
-    Input,
     FormFeedback,
-    FormText,
-    CardBody,
-    Card,
-    Button,
-    Col,
-    Row,
-    Modal,
+    Input,
+    Table,
+    Pagination,
+    PaginationItem,
+    PaginationLink, Row, Col, Container, Modal,
     ModalHeader,
     ModalBody,
-    ModalFooter
 } from 'reactstrap';
 
 import { Formik, Field, ErrorMessage } from 'formik';
@@ -44,16 +43,53 @@ class EditCalculate extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+
         }
     }
 
-    
+
     render() {
         return (
-            <div>
+            <Container fluid={true} style={{ backgroundColor: '#B3D864' }} >
+                <Row>
+                    <Col >
+                        รหัสสินค้า: {this.props.product.productID}
+                    </Col>
+                    <Col >
+                        รายการสินค้า: {this.props.product.productName}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col >
+                        EOQ   =   SQR(2DO / UC)
+                    </Col>
+                </Row>
+                <Row>
+                    <Col >
+                        D ความต้องการสินค้าในเวลา 1 ปี
+                    </Col>
+                </Row>
+                <Row>
+                    <Col >
+                        O ค่าใช้จ่ายในการสั่งซื้อต่อครั้ง
+                    </Col>
+                </Row>
+                <Row>
+                    <Col >
+                        U ต้นทุนของสินค้าต่อหน่วย
+                    </Col>
+                </Row>
+                <Row>
+                    <Col >
+                        C ค่าใช้จ่ายในการเก็บรักษาสินค้าคิดเป็น % ของมูลค่าสินค้าทั้งปี
+                    </Col>
+                </Row>
+                <Row>
+                    
+                </Row>
+            </Container>
 
-            </div >
+
         );
     }
 }
